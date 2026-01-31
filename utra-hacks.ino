e5ca12cfe6e8aa9e2fd0c3dfd9bde8b83c12e9e2
smash <skaftasoneric@gmail.com>
2026-01-31 15:57:17 -0500

    :(

--- utra-hacks.ino
+++ utra-hacks.ino
@@ -3,11 +3,11 @@
 
 class ColourSensor {
   private:
-    int S0 = 8;
-    int S1 = 9;
-    int S2 = 10;
-    int S3 = 11;
-    int OUT = 12;
+    int S0;
+    int S1;
+    int S2;
+    int S3;
+    int OUT;
 
     void init() {
       pinMode(S0, OUTPUT);
@@ -15,6 +15,10 @@ class ColourSensor {
       pinMode(S2, OUTPUT);
       pinMode(S3, OUTPUT);
       pinMode(OUT, INPUT);
+
+      // Frequency scaling 20%
+      digitalWrite(S0, HIGH);
+      digitalWrite(S1, LOW);
     }
 
 
@@ -30,11 +34,11 @@ class ColourSensor {
     }
 
     ColourSensor() {
-        S0 = 8;
-        S1 = 9;
+        S0 = 12;
+        S1 = 11;
         S2 = 10;
-        S3 = 11;
-        OUT = 12;
+        S3 = 9;
+        OUT = 8;
 
         init();
     }
@@ -47,27 +51,27 @@ class ColourSensor {
       // freq is inverse of period or 1 / (duration of low + duration of high)
       // uses 1 million to convert from microseconds to seconds
       float redPeriod = pulseIn(OUT, LOW) + pulseIn(OUT, HIGH);
-      float redFreq = 1000000.0 / period; // frequency in Hz
+      float redFreq = 1000000.0 / redPeriod; // frequency in Hz
 
       // Get Green
       digitalWrite(this->S2, LOW);
       digitalWrite(this->S3, HIGH);
       float greenPeriod = pulseIn(OUT, LOW) + pulseIn(OUT, HIGH);
-      float greenFreq = 1000000.0 / period;
+      float greenFreq = 1000000.0 / greenPeriod;
 
 
       // Get Blue
       digitalWrite(this->S2, HIGH);
       digitalWrite(this->S3, HIGH);
       float bluePeriod = pulseIn(OUT, LOW) + pulseIn(OUT, HIGH);
-      float blueFreq = 1000000.0 / period;
+      float blueFreq = 1000000.0 / bluePeriod;
 
       return {redFreq, greenFreq, blueFreq};
     }
 
-    void read(int &red, int &green, int &blue);
 };
 
+ColourSensor sensor;
 
 void setup() {
   Serial.begin(9600);
